cc97a8038df4d381ccae4d2b543120ae275e9cf6
nehanalinik <nehanalinik@gmail.com>
2022-12-10 00:00:25 +0530

    added images

--- src/helper/BestSellersHelper.js
+++ src/helper/BestSellersHelper.js
@@ -1,9 +1,9 @@
-import img1 from "../assets/Cosmetics Jar Mockup.png";
-import img2 from "../assets/Serum.png";
-import img3 from "../assets/Shampoo.png";
-import img4 from "../assets/Mockup4.png";
-import img5 from "../assets/BannerTubes.png";
-import img6 from "../assets/Combo Mockup.png";
+import img1 from "../assets/nightCream.jpg";
+import img2 from "../assets/Serum.jpg";
+import img3 from "../assets/Shampoo.jpg";
+import img4 from "../assets/showerGel.jpg";
+import img5 from "../assets/comboTube.jpg";
+import img6 from "../assets/comboPack.jpg";
 
 const BestSellersHelper = [
   {
